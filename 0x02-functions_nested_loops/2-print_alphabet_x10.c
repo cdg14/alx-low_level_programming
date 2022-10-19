1f28329d1186d8ef902902a2f54ad2b37f565528
Nnamdi Cletus <nnamdiokpalaugo@gmail.com>
2022-10-19 13:30:30 +0100

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabets
+ *
+ * Return: always 0 (success)
+ */
+
+void print_alphabet_x10(void)
+{
+	char i;
+	int j;
+
+	for (j = 0; j <= 9; j++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
