c9a484ddd705c87c934f97dc199668666040d9ff
Nnamdi Cletus <nnamdiokpalaugo@gmail.com>
2022-11-25 14:11:33 +0100

    linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,40 +1,29 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "lists.h"
 
 /**
- * main - check the code
+ * print_list - prints all the elements of a list_t list.
+ * @h: the list
  *
- * Return: Always 0.
+ * Return: the number of nodes
  */
-int main(void)
+size_t print_list(const list_t *h)
 {
-    list_t *head;
-    list_t *new;
-    list_t hello = {"World", 5, NULL};
-    size_t n;
+	size_t nodes = 0;
 
-    head = &hello;
-    new = malloc(sizeof(list_t));
-    if (new == NULL)
-    {
-        printf("Error\n");
-        return (1);
-    }
-    new->str = strdup("Hello");
-    new->len = 5;
-    new->next = head;
-    head = new;
-    n = print_list(head);
-    printf("-> %lu elements\n", n);
+	while (h)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+			h = h->next;
+			nodes++;
+			continue;
+		}
 
-    printf("\n");
-    free(new->str);
-    new->str = NULL;
-    n = print_list(head);
-    printf("-> %lu elements\n", n);
+		printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		nodes++;
+	}
 
-    free(new);
-    return (0);
+	return (nodes);
 }
