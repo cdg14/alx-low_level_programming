f4684f5bc1e4e92b8da1c46f2df8fd51c285667f
Nnamdi Cletus <nnamdiokpalaugo@gmail.com>
2023-01-13 09:37:55 +0100

    doubling linked lists

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,21 +1,21 @@
 #include "lists.h"
 
 /**
- * print_dlistint - Prints all the elements of a dlistint_t list.
- * @h: The head of the dlistint_t list.
+ * print_dlistint - prints a list
  *
- * Return: The number of nodes in the list.
+ * @head: list head
+ * Return: length of list
  */
-size_t print_dlistint(const dlistint_t *h)
+
+size_t print_dlistint(const dlistint_t *head)
 {
-	size_t nodes = 0;
+	size_t count = 0;
 
-	while (h)
+	while (head)
 	{
-		nodes++;
-		printf("%d\n", h->n);
-		h = h->next;
+		printf("%d\n", head->n);
+		count++;
+		head = head->next;
 	}
-
-	return (nodes);
+	return (count);
 }
